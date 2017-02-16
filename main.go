cb19d966c184d778894e844cb56dcf888f8de454
Rohan Allison <rohanthewiz_at_gmail.com>
2017-02-16 14:10:10 -0600

    Add a route level middleware

--- main.go
+++ main.go
@@ -13,9 +13,20 @@ func main() {
 		return c.String(http.StatusOK, "pong")
 	})
 
+	e.GET("/users", func(c echo.Context) error {
+		return c.String(http.StatusOK, "/users")
+	}, trackUsers)	
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func rootHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func trackUsers(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		println("A request is being sent to /users")
+		return next(c)
+	}
 }
