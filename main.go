85ec24cc5c9f16569e821d86e9aa184f61b0eede
Rohan Allison <rohanthewiz_at_gmail.com>
2017-02-16 14:15:14 -0600

    Put users handler in a named method

--- main.go
+++ main.go
@@ -9,19 +9,24 @@ func main() {
 	e := echo.New()
 
 	e.GET("/", rootHandler)
+	// Just to show we can use an anonymous function for handling
 	e.GET("/ping", func(c echo.Context) error {
 		return c.String(http.StatusOK, "pong")
 	})
 
-	e.GET("/users", func(c echo.Context) error {
-		return c.String(http.StatusOK, "/users")
-	}, trackUsers)	
+	e.GET("/users", usersRoot, trackUsers)	
 
+	// Start listening
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
+
 func rootHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func usersRoot(c echo.Context) error {
+	return c.String(http.StatusOK, "We would return a listing of users here")
 }
 
 func trackUsers(next echo.HandlerFunc) echo.HandlerFunc {
